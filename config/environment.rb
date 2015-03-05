702dd84feba28f1361121aa1b8f93493459ce1ef
Seiji Naganuma <Snaganumas@gmail.com>
2015-03-04 22:57:34 -0500

    modifications for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -17,7 +17,7 @@ end
 configure :production do
   require_all 'app'
 
-  ENV['DATABASE_URL'] = "postgres://lczwxjjhixpfmn:2uaK3d6aye7UFR_sKTtrKs52QA@ec2-50-19-249-214.compute-1.amazonaws.com:5432/d39i70q4p7k6uq"
+  ENV['DATABASE_URL'] = "postgres://svmphxasdepskl:9CFMIbfBAAUSF3D8lXB9Ph19OG@ec2-50-19-249-214.compute-1.amazonaws.com:5432/d6um3v6k5ksqsp"
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
   ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
